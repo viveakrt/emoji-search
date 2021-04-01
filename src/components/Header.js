11bbab8dd37d82e9efd0c575f140a2c139bdbaad
Vivek <vivek.15@mountblue.tech>
2021-04-01 14:20:41 +0530

    Added Header component with search bar

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Header extends React.Component  {
+    render() {
+        return (
+            <div className="searchIconCircle material-icons" >
+            <input type="text"  />
+                <b className="searchIcon">search</b>
+                
+            </div>
+        )
+    }
+}
+export default Header;
