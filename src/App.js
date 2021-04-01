1887297e5b92d67fae7bfcdbdc58accb922e4b42
Vivek <vivek.15@mountblue.tech>
2021-04-01 16:08:52 +0530

    Added json data for emoji

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, {
   Component
 } from "react";
-import Header from "./components/Header"
+import InputSearch from "./components/InputSearch"
+import SearchEmoji from "./components/SearchEmoji"
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredEmoji: SearchEmoji("", 20)
+    };
+  }
+
+
+  handleSearchChange = event =>{
+    this.setState({
+      SearchEmoji: SearchEmoji(event.target.value,20)
+    });
+  };
+
   render (){
     return (
-      <Header />
+      <InputSearch textChange={this.handleSearchChange} />
     );
   }
 }
