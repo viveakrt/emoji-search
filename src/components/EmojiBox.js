e222f8566ccc6b6b7e0fc92b000283a35bd553b6
Vivek <vivek.15@mountblue.tech>
2021-04-02 02:21:19 +0530

    Added npm module react-copy-to-clipboard and written code for copy

--- src/components/EmojiBox.js
+++ src/components/EmojiBox.js
@@ -1,20 +1,29 @@
 import PropTypes from "prop-types";
 import React, {Component} from "react";
-
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 class EmojiBox extends Component {
     static propTypes = {
         title: PropTypes.string,
         Symbol:PropTypes.string
     };
 
+    state = {
+        copied: false,
+      };
+
     render() {
         const HexCode = this.props.symbol.codePointAt(0).toString(16);
         const src = `//cdn.jsdelivr.net/emojione/assets/png/${HexCode}.png`;
         return (
-            <div className="emojiBox" >
+            <CopyToClipboard text={this.props.symbol}
+          onCopy={() => this.setState({copied: true})}>
+          <div className="emojiBox" >
+                
                 <img alt={this.props.title} src={src} />
                 <span className="title">{this.props.title}</span>
             </div>
+        </CopyToClipboard>
+            
             );
     }
 }
