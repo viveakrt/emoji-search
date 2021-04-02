0844567820aaf21a0e34e0be55e75e2de29038bb
Vivek <vivek.15@mountblue.tech>
2021-04-02 09:59:07 +0530

    Added media query for mobile devices and fix issue

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      SearchEmoji: SearchEmoji("", 1000)
+      SearchEmoji: SearchEmoji("", 600)
     };
   }
 
